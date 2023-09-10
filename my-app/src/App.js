8f2bf2e6cad068ee108f02922a2ccf74dc7fd424
Dylan Lai <dylanlai@Dylans-MacBook-Pro.local>
2023-09-10 19:14:48 -0400

    Changed center and added comments and added other file formats(.json, .zip)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -20,25 +20,34 @@ class App extends Component {
 
     if (selectedFile) {
       alert(`File selected: ${selectedFile.name}`);
+      //get file type
+        //read last few characters?
+      //read data
+        //geoJson has label-x and label-y
+      //rerender with data
     } else {
       alert('Please select a file.');
     }
   };
 
+  //.zip for shapefiles with multiple files
+  //.json for geoJson
   render() {
+    let labelx = 138.44217;
+    let labely = 36.142538;
     return (
       <div>
         <h1>Please Upload a Map file</h1>
-        <h3>.shp .geojson or .kml</h3>
-        <input type="file" accept=".shp, .geojson, .kml" onChange={this.handleFileChange} />
+        <h3>.shp, .zip, .json, .geojson, or .kml</h3>
+        <input type="file" accept=".zip, .shp, .json, .geojson, .kml" onChange={this.handleFileChange} />
         <button onClick={this.handleUpload}>Upload</button>
 
-        <MapContainer center={[0, 0]} zoom={2} scrollWheelZoom={true}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-</MapContainer>
+        <MapContainer center={[labely, labelx]} zoom={5} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
       </div>
     );
   }
