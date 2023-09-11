e43e202f515661c09f81021c9cdd92e93f84c177
Dylan Lai <dylanlai@Dylans-MacBook-Pro.local>
2023-09-10 20:20:09 -0400

    combined buttons and started parsing geoJSONs which are formatted JSONs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,22 +13,30 @@ class App extends Component {
   }
 
   handleFileChange = (event) => {
+    var reader = new FileReader();
     const selectedFile = event.target.files[0];
+    reader.readAsText(selectedFile);
+    reader.onload = this.handleUpload;
     this.setState({ selectedFile });
     if (selectedFile) {
       alert(`File selected: ${selectedFile.name}`);
+      console.log(selectedFile);
     }
   };
 
-  handleUpload = () => {
+  handleUpload = (event) => {
     const { selectedFile } = this.state;
-
+    alert(selectedFile);
     if (selectedFile) {
       if(selectedFile.name.endsWith('.shp')) {
         //Handle shp parsing
       }
       else if (selectedFile.name.endsWith('.json')) {
         //handle GeoJSON parsing
+        console.log(event.target.result);
+        var parsed = JSON.parse(event.target.result);
+        alert(parsed);
+        //geoJSon has label-x and label-y
       }
       else if (selectedFile.name.endsWith('.kml')) {
         //handle kml parsing
@@ -52,7 +60,7 @@ class App extends Component {
         <h1>Please Upload a Map file</h1>
         <h3>.shp, .json, .geojson, or .kml</h3>
         <input type="file" accept=".shp, .json, .geojson, .kml" onChange={this.handleFileChange} />
-        <button onClick={this.handleUpload}>Upload</button>
+        {/* <button onClick={this.handleUpload}>Upload</button> */}
 
         <MapContainer center={[labely, labelx]} zoom={5} scrollWheelZoom={true}>
           <TileLayer
