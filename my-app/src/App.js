c74726fd879c6bb29bf1b38f287ced8b5df7bf3c
Kevin <kevin_brito@stonybrook.edu>
2023-09-11 12:43:04 -0400

    GeoJSON loading

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
@@ -10,6 +10,7 @@ class App extends Component {
       selectedFile: null,
       mapData: null,
     };
+    this.mapRef = React.createRef();
   }
 
   handleFileChange = (event) => {
@@ -20,49 +21,84 @@ class App extends Component {
     this.setState({ selectedFile });
     if (selectedFile) {
       alert(`File selected: ${selectedFile.name}`);
-      console.log(selectedFile);
     }
   };
 
   handleUpload = (event) => {
     const { selectedFile } = this.state;
-    alert(selectedFile);
     if (selectedFile) {
-      if(selectedFile.name.endsWith('.shp')) {
-        //Handle shp parsing
+      if (selectedFile.name.endsWith('.shp')) {
+        // Handle shp parsing
+        alert('Parsing Shapefile');
+        // Update this.state.mapData with the parsed data
+      } else if (selectedFile.name.endsWith('.json') || selectedFile.name.endsWith('.geojson')) {
+        // Handle GeoJSON parsing
+        alert('Parsing GeoJSON');
+        const parsedData = JSON.parse(event.target.result); // Parse the JSON data
+        console.log(parsedData);
+        this.setState({ mapData: parsedData }, () => {
+          this.renderMap();
+        });
+        this.renderMap();
+        // Update this.state.mapData with the parsed data
+      } else if (selectedFile.name.endsWith('.kml')) {
+        // Handle KML parsing
+        alert('Parsing KML');
+        // Update this.state.mapData with the parsed data
+      } else {
+        alert('Please upload the specified filetype');
       }
-      else if (selectedFile.name.endsWith('.json')) {
-        //handle GeoJSON parsing
-        console.log(event.target.result);
-        var parsed = JSON.parse(event.target.result);
-        alert(parsed);
-        //geoJSon has label-x and label-y
-      }
-      else if (selectedFile.name.endsWith('.kml')) {
-        //handle kml parsing
-      }
-      else {
-        alert("Please upload the specified filetype");
-      }
-      //rerender with data
-    } 
-    else {
+      // Rerender the map with data if needed
+      this.renderMap();
+    } else {
       alert('Please select a file');
     }
   };
 
+  // Function to render or update the map with new data
+renderMap = () => {
+  // Get the map instance
+  const map = this.mapRef.current;
+  if (map) {
+    // Remove the previous GeoJSON layer if it exists
+    if (this.geoJsonLayer) {
+      map.removeLayer(this.geoJsonLayer);
+    }
+
+    // Render the map data
+    const { mapData } = this.state;
+    if (mapData) {
+      // Create a new GeoJSON layer
+      this.geoJsonLayer = L.geoJSON(mapData);
+
+      // Add the base TileLayer
+      const tileLayer = L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }
+      ).addTo(map);
+
+      // Add the new GeoJSON layer on top of the base TileLayer
+      this.geoJsonLayer.addTo(map);
+
+      // Fit the map to the bounds of the GeoJSON layer
+      map.fitBounds(this.geoJsonLayer.getBounds());
+    }
+  }
+};
+
+
+
   //.json for geoJson
   render() {
-    let labelx = 138.44217;
-    let labely = 36.142538;
     return (
       <div>
         <h1>Please Upload a Map file</h1>
         <h3>.shp, .json, .geojson, or .kml</h3>
         <input type="file" accept=".shp, .json, .geojson, .kml" onChange={this.handleFileChange} />
-        {/* <button onClick={this.handleUpload}>Upload</button> */}
 
-        <MapContainer center={[labely, labelx]} zoom={5} scrollWheelZoom={true}>
+        <MapContainer ref={this.mapRef} center={[0, 0]} zoom={2} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
