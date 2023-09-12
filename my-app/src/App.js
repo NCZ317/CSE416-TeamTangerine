955912f93d641841d09be7bfd0885a0c7904e051
Kevin <kevin_brito@stonybrook.edu>
2023-09-12 12:02:37 -0400

    Fail to launch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import shp from 'shpjs'
 
 class App extends Component {
   constructor() {
