8879355bc55bc9408ed91c2bea586093da04d5be
Kevin <kevin_brito@stonybrook.edu>
2023-11-03 19:41:58 -0400

    Homescreen started, might need to rewrite mapcards

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div className="App">
       <HomeWrapper/>
-      <CreateAccountModal/>
+      {/* <CreateAccountModal/> */}
     </div>
   );
 }
--- client/src/components/HomeWrapper.js
+++ client/src/components/HomeWrapper.js
@@ -1,12 +1,49 @@
-import AppBanner from "./AppBanner";
-import MapCard from "./MapCard";
-import MapWrapper from "./MapWrapper";
+import React from 'react';
+import { Box, Button, Grid } from '@mui/material';
+import AppBanner from './AppBanner';
+import MapCard from './MapCard';
+import SortIcon from '@mui/icons-material/Sort';
+
+const searchbarStyle = {
+    backgroundColor: '#F6A440',
+    color: '#fff',
+    padding: '12px 16px',
+    border: 'none',
+    width: '95%',
+    height: '24px'
+};
+
+const buttonStyle = {
+    color: '#79C200',
+    border: 'none',
+    width: '5%',
+    height: '49px'
+};
 
 export default function HomeWrapper() {
     return (
-            <div>
-                <AppBanner />
-
+        <div>
+            <AppBanner />
+            
+            <div style={{ display: 'flex'}}>
+                <input type="text" placeholder="Search" style={searchbarStyle} className="searchbar" />
+                <Button
+                    variant="text"
+                    color="primary"
+                    startIcon={<SortIcon />}
+                    style={buttonStyle}
+                />
             </div>
-        );
+            
+            <Box mt={2}>
+                <Grid container spacing={1}>
+                    {[1, 2, 3, 4, 5, 6].map((cardId) => (
+                        <Grid item key={cardId} xs={12} sm={6}>
+                            <MapCard />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Box>
+        </div>
+    );
 }
