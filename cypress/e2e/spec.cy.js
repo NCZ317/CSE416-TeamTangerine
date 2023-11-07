e0fbadf27fe0154e7b53c8e3e4e836c799fbdf87
Kevin <kevin_brito@stonybrook.edu>
2023-11-07 15:52:34 -0500

    cypress tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,19 @@
-describe('Simple Test 1', () => {
-  it('Site exists', () => {
-    cy.visit('https://terratrove-df08dd7fc1f7.herokuapp.com/')
-    cy.url().should('include', '/')
-  })
-})
-describe('Simple Test 2', () => {
-  it('Is TerraTrove', () => {
-    cy.visit('https://terratrove-df08dd7fc1f7.herokuapp.com/')
-    cy.contains('Map')
-  })
-})
+describe('Login Modal Test', () => {
+  it('Opens the page and clicks on the Login button', () => {
+    cy.visit('https://terratrove-df08dd7fc1f7.herokuapp.com/'); 
+    cy.get('#login-button').click(); // Replace with the actual ID or selector of your Login button
+
+    // Check that the Login Modal is visible
+    cy.get('#login-modal').should('be.visible'); // Replace with the actual ID or selector of your Login Modal
+  });
+});
+
+describe('Create Account Modal Test', () => {
+  it('should open Create Account modal from Login modal', () => {
+    cy.visit('https://terratrove-df08dd7fc1f7.herokuapp.com/');
+    cy.get('#login-button').click();
+    cy.get('#login-modal').should('be.visible');
+    cy.get('#sign-up-button').click();
+    cy.get('#create-account-modal').should('be.visible');
+  });
+});
