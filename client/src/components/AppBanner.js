f09f67cf2559e6891ef71ef5ae239cdb75604893
Kevin <kevin_brito@stonybrook.edu>
2023-11-15 19:58:03 -0500

    AppBanner CreateAccountModal and Login Modal CSS

--- client/src/components/AppBanner.js
+++ client/src/components/AppBanner.js
@@ -7,7 +7,6 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import logo from './logo.png';
 import LoginModal from './LoginModal';
 import CreateMapModal from './CreateMapModal';
@@ -26,23 +25,6 @@ const theme = createTheme({
   },
 });
 
-const logoStyle = {
-  fontSize: '36px',
-  fontWeight: 'bold',
-  color: '#7ac100',
-  textShadow: '3px  3px  15px  black',
-};
-
-const buttonContainerStyle = {
-  display: 'flex',
-  marginLeft: 'auto', // Align buttons to the right
-};
-
-const buttonStyle = {
-  marginLeft: '30px', // Adjust margin as needed between buttons
-  fontWeight: 'bold',
-};
-
 const AppBanner = () => {
   const { auth } = useContext(AuthContext);
   const { store } = useContext(GlobalStoreContext);
@@ -51,24 +33,19 @@ const AppBanner = () => {
   const [anchorEl, setAnchorEl] = useState(null);
 
   const handleCreateMap = () => {
-    //alert('CREATE MAP clicked');
-    // Change state
-    store.setScreen("MAP_EDITOR")
+    store.setScreen("MAP_EDITOR");
     setCreateMapModalOpen(true);
   };
 
   const handleLogin = () => {
-    // Open the LoginModal
     setLoginModalOpen(true);
   };
 
   const handleCloseLoginModal = () => {
-    // Close the LoginModal
     setLoginModalOpen(false);
   };
 
   const handleCloseCreateMapModal = () => {
-    // Close the CreateMapModal
     setCreateMapModalOpen(false);
   };
 
@@ -81,19 +58,12 @@ const AppBanner = () => {
   };
 
   const handleLogout = () => {
-    // Call your logout function from the AuthContext
     auth.logoutUser();
-    // Close the menu
     handleAccountMenuClose();
   };
 
   const handleUserProfile = () => {
-    //SHOULD ROUTE TO THE USER'S PROFILE
-    // store.currentScreen = "USER";
     store.setScreen("USER");
-    // console.log("currentScreen : "+ store.currentScreen);
-    // alert("To Profile");
-    // console.log("To Profile");
     handleAccountMenuClose();
   }
 
@@ -106,7 +76,7 @@ const AppBanner = () => {
             edge="end"
             aria-label="account of current user"
             aria-haspopup="true"
-            style={{ margin: '0px 0px 0px 24px', backgroundColor: '#D9D9D9', color: '#000' }}
+            id='account-button'
             onClick={handleAccountMenuClick}
           >
             <div>{auth.getUserInitials()}</div>
@@ -127,18 +97,17 @@ const AppBanner = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{ flexGrow: 1 }}>
+      <Box className="app-banner" sx={{ flexGrow: 1 }}>
         <AppBar position="static" color="header">
           <Toolbar>
-            <a href='/'><img src={logo} height="50px" style={{ marginBottom: '10px', marginTop: '10px' }} /></a>
-            <div style={buttonContainerStyle}>
+            <a href='/' className="logo-link"><img src={logo} alt='map img' height="50px" className="app-logo" /></a>
+            <div className="button-container">
               {auth.loggedIn ? (
                 <>
                   <Button
                     color="leaves"
                     variant="text"
-                    style={buttonStyle}
-                    id="create-map-button"
+                    className="create-map-button"
                     onClick={handleCreateMap}
                   >
                     Create Map
@@ -147,7 +116,7 @@ const AppBanner = () => {
                 </>
               ) : (
                 <>
-                  <Button color="leaves" variant="text" style={buttonStyle} id="login-button" onClick={handleLogin}>
+                  <Button color="leaves" variant="text" className="login-button" onClick={handleLogin}>
                     Login
                   </Button>
                 </>
