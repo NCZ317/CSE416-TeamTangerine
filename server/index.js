05ed7b1ff57731022c2ff3862cc3c104522bcb4a
Dylan Lai <dylan.lai@stonybrook.edu>
2023-11-18 18:31:58 -0500

    Added map-router pathing

--- server/index.js
+++ server/index.js
@@ -27,6 +27,8 @@ app.use(cookieParser())
 // SETUP OUR OWN ROUTERS AS MIDDLEWARE
 const authRouter = require('./routes/auth-router')
 app.use('/auth', authRouter)
+const mapRouter = require('./routes/map-router')
+app.use('/store', mapRouter)
 // const mapRouter = require('./routes/map-router')
 // app.use('/api', mapRouter)
 
