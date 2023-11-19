3ce8b37b04db1391a25accd82ebe9bde309e7032
Kevin <kevin_brito@stonybrook.edu>
2023-11-19 10:32:08 -0500

    jsTPS (undo redo) file added

--- client/src/store/store-request-api/index.js
+++ client/src/store/store-request-api/index.js
@@ -1,70 +1,56 @@
 import axios from 'axios'
 axios.defaults.withCredentials = true;
 const baseURL = process.env.NODE_ENV === 'production'
-  ? 'https://terratrove-df08dd7fc1f7.herokuapp.com/api'
-  : 'http://localhost:4000/api';
+  ? 'https://terratrove-df08dd7fc1f7.herokuapp.com/auth'
+  : 'http://localhost:4000/auth';
+
 const api = axios.create({
-    baseURL,
+  baseURL,
 });
-export const createMap = (title, jsonData, mapType, ownerEmail) => {
-    return api.post('/map', {
-        title : String,
-        description : String,
-        ownerEmail : String,
-        username : String,
-        jsonData : Object,
-        mapType : String,
-        mapLayers : [],
+
+
+export const createMap =(title, jsonData, mapType, ownerEmail, username) => {
+    return api.post(`map`, {
+        title: title,
+        description: '',
+        ownerEmail: ownerEmail,
+        username: username,
+        jsonData: jsonData,
+        mapType: mapType,
+        mapLayers: [],
         likes: 0,
         views: 0,
-        comments: {
-            user: String,
-            message: String },
-        published: Boolean,
-        publishedDate: Date 
+        comments: [],
+        published: false,
+        publishedDate: new Date(),
     })
 }
-export const deleteMapById=(id) =>{
-    return api.delete('/map/delete/${id}',{
-        _id: Object
+
+export const deleteMapById = (id) => api.delete(`/map/${id}`)
+
+export const getMapById = (id) => api.get(`/map/${id}`)
+
+export const  getMapPairs = () => api.get(`/mappairs/`)
+
+export const updateMapById = (id, map) => {
+    return api.put(`/map/${id}`, {
+        map: map
     })
-}
-export const getMapById=(id) =>{
-    return api.get('/map/${id}',{
-        _id: Object
-    })
-}
-export const getMapPairs=() =>{
-    return api.get('map/pairs',{})
-}
-export const updateMapById=(id) =>{
-    return api.put('/map/update/${id}',{
-           map: JSON,
-           _id: Object
-     })
-}
-export const getMapsByKeyword=(keyword) =>{
-    return api.get('/map/keyword/${keyword}',{ 
-          keyword: String
-    })
-}
-export const getMapsByUser=(username) =>{
-     return api.get('/maps/username/${username',{})
-}
-export const getMaps=() =>{
-     return api.get('/maps',{})
-}
+} 
+
+export const getMaps = () => api.get(`/maps`)
+
+//GET MAP BY KEYWORD AND USER TO BE DONE LATER
+
 const apis = {
     createMap,
     deleteMapById,
     getMapById,
     getMapPairs,
     updateMapById,
-    getMapsByKeyword,
-    getMapsByUser,
     getMaps,
 }
+
 export default apis
-//CREATE ALL THE REQUESTS
 
 
