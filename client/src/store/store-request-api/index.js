e9a09d2ee6a40704e5d444429578a581aaed19e8
Kevin <kevin_brito@stonybrook.edu>
2023-11-18 19:34:58 -0500

    backend store controller updates & front end store api implemented

--- client/src/store/store-request-api/index.js
+++ client/src/store/store-request-api/index.js
@@ -1,14 +1,46 @@
-
 import axios from 'axios'
 axios.defaults.withCredentials = true;
+const baseURL = process.env.NODE_ENV === 'production'
+  ? 'https://terratrove-df08dd7fc1f7.herokuapp.com/auth'
+  : 'http://localhost:4000/auth';
+
 const api = axios.create({
-    // baseURL: 'https://terratrove-df08dd7fc1f7.herokuapp.com/api'
-    baseURL: 'http://localhost:4000/api'
-})
+  baseURL,
+});
 
 
-//CREATE ALL THE REQUESTS
+export const createMap =(title, jsonData, mapType, ownerEmail, username) => {
+    return api.post(`map`, {
+        title: title,
+        description: '',
+        ownerEmail: ownerEmail,
+        username: username,
+        jsonData: jsonData,
+        mapType: mapType,
+        mapLayers: [],
+        likes: 0,
+        views: 0,
+        comments: [],
+        published: false,
+        publishedDate: new Date(),
+    })
+}
 
+export const deleteMapById = (id) => api.delete(`/map/${id}`)
+
+export const getMapById = (id) => api.get(`/map/${id}`)
+
+export const  getMapPairs = () => api.get(`/mappairs/`)
+
+export const updateMapById = (id, map) => {
+    return api.put(`/map/${id}`, {
+        map: map
+    })
+} 
+
+export const getMaps = () => api.get(`/maps`)
+
+//GET MAP BY KEYWORD AND USER TO BE DONE LATER
 
 const apis = {
 
