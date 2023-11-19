e9414426dd14968c108eeab77ab64fb1e3e0a4a3
Dylan Lai <dylan.lai@stonybrook.edu>
2023-11-18 20:06:18 -0500

    Added map-router pathing

--- client/src/store/store-request-api/index.js
+++ client/src/store/store-request-api/index.js
@@ -8,41 +8,68 @@ axios.defaults.withCredentials = true;
 const baseURL = process.env.NODE_ENV === 'production'
   ? 'https://terratrove-df08dd7fc1f7.herokuapp.com/api'
   : 'http://localhost:4000/api';
-
-
-//CREATE ALL THE REQUESTS
- //Process getting information from server
-    //Process getting maps from server, returns array of ids
-        /* store.getMaps = async function() {
-            try {
-                const response = await api.loginUser(email, password);
-                if (response.status === 200) {
-                    authReducer({
-                        type: GlobalStoreActionType.LOAD_CURRENT_MAPS,
-                        payload: {
-                            user: response.data.user
-                        }
-                    })
-                    navigate("/");
-                    // history.push("/");
-                }
-            } catch (error) {
-                console.log(error.response.data.errorMessage);
-                authReducer({
-                    type: AuthActionType.ERROR,
-                    payload: {
-                        errorMessage: error.response.data.errorMessage
-                    }
-                });
-            }
-        } */
-    //Process getting users from server
-        //store.getUsers = function()
-
-const apis = {
-
+const api = axios.create({
+    baseURL,
+});
+export const createMap = (title, jsonData, mapType, ownerEmail) => {
+    return api.post('/map', {
+        title : String,
+        description : String,
+        ownerEmail : String,
+        username : String,
+        jsonData : Object,
+        mapType : String,
+        mapLayers : [],
+        likes: 0,
+        views: 0,
+        comments: {
+            user: String,
+            message: String },
+        published: Boolean,
+        publishedDate: Date 
+    })
 }
-
+export const deleteMapById=(id) =>{
+    return api.delete('/map/delete/${id}',{
+        _id: Object
+    })
+}
+export const getMapById=(id) =>{
+    return api.get('/map/${id}',{
+        _id: Object
+    })
+}
+export const getMapPairs=() =>{
+    return api.get('map/pairs',{})
+}
+export const updateMapById=(id) =>{
+    return api.put('/map/update/${id}',{
+           map: JSON,
+           _id: Object
+     })
+}
+export const getMapsByKeyword=(keyword) =>{
+    return api.get('/map/keyword/${keyword}',{ 
+          keyword: String
+    })
+}
+export const getMapsByUser=(username) =>{
+     return api.get('/maps/username/${username',{})
+}
+export const getMaps=() =>{
+     return api.get('/maps',{})
+}
+const apis = {
+    createMap,
+    deleteMapById,
+    getMapById,
+    getMapPairs,
+    updateMapById,
+    getMapsByKeyword,
+    getMapsByUser,
+    getMaps,
+}
 export default apis
+//CREATE ALL THE REQUESTS
 
 
